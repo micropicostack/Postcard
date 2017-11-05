7c195463ab8e796140e204e43d10fe31626716eb
Valitseja <leppika@gmail.com>
2017-11-05 22:58:11 +0200

    Fixed exception naming and added documentation

--- postcard/mailer.py
+++ postcard/mailer.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """."""
 import smtplib
 
@@ -20,18 +21,20 @@ class Mailman:
         self.server = None
 
     def connect(self):
-        """."""
+        """Connects to the specified host at the specified port.
+        Raises ConnectionError if not possible."""
         try:
             self.server = smtplib.SMTP(self.host, self.port)
         except smtplib.socket.gaierror:
             raise ConnectionError("Error connecting to %s" % (self.host))
 
     def auth(self):
-        """."""
+        """Authendicates with the mail server. Raises MailmanAuthError
+        when the authentication fails."""
         try:
             self.server.login(self.username, self.password)
         except smtplib.SMTPAuthenticationError:
-            raise AuthError("Invalid username (%s) and/or password" % (self.username))
+            raise MailmanAuthError("Invalid username (%s) and/or password" % (self.username))
 
     def deliver(self, sender, recipients, message):
         """."""
@@ -41,6 +44,6 @@ class Mailman:
         try:
             self.server.sendmail(sender, recipients, message.as_string())
         except smtplib.SMTPDataError as errormsg:
-            raise SendError("Couldn't send message: %s" % (errormsg))
+            raise MailmanSendError("Couldn't send message: %s" % (errormsg))
         except smtplib.socket.timeout:
             raise ConnectionError("Socket error while sending message")
