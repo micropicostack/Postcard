f70d17b2b5b08a86018fd9f4b3a0dcd82c91eb3e
Valitseja <leppika@gmail.com>
2017-11-06 16:23:02 +0200

    Added documentation

--- postcard/postcard.py
+++ postcard/postcard.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 """Description"""
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
@@ -18,7 +19,11 @@ class Postcard():
         self.message = None
 
     def create(self, txt_msg, hmtl_msg):
-        """."""
+        """Create the multipart email message
+        Args:
+            txt_msg (str): message in plain text format
+            html_msg (str): message in html format
+        """
         self.message = MIMEMultipart('related')
 
         self.message['Subject'] = self.subject
@@ -34,18 +39,28 @@ class Postcard():
         msg_alt.attach(MIMEText(hmtl_msg, 'html'))
 
     def add_image(self, image):
-        """."""
-        key, file_path = image
-        with open(file_path, 'rb') as f:
-            img = f.read()
-        cid = MIMEImage(img)
-        cid.add_header('Content-ID', key)
-        self.message.attach(cid)
+        """Adds an image to the message in the cid format.
+        Args:
+            image (tuple): the image should be in the (key, path) format
+        Raises:
+            ValueError when the file does not exist
+        """
+        key, filepath = image
+        abspath = os.path.abspath(filepath)
+
+        if os.path.exists(abspath):
+            with open(abspath, 'rb') as image_file:
+                img = image_file.read()
+            cid = MIMEImage(img)
+            cid.add_header('Content-ID', key)
+            self.message.attach(cid)
+        else:
+            raise ValueError("The path %s does not exist!" % abspath)
 
     def add_file(self):
         """."""
         pass
 
     def package(self):
-        """."""
+        """Return the email message as a string"""
         return self.message.as_string()
