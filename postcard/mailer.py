0d9ae61cbacde35ec3b60c94017a6303c34dcec9
Valitseja <leppika@gmail.com>
2017-11-06 16:23:15 +0200

    Added documentation

--- postcard/mailer.py
+++ postcard/mailer.py
@@ -4,15 +4,15 @@ import smtplib
 
 
 class MailmanAuthError(Exception):
-    """."""
+    """Authentication error"""
 
 
 class MailmanSendError(Exception):
-    """."""
+    """The SMTP server didn't accept the data."""
 
 
 class Mailman:
-    """."""
+    """This class manages email sending."""
     def __init__(self, host, port, username=None, password=None):
         self.host = host
         self.port = port
@@ -22,22 +22,34 @@ class Mailman:
 
     def connect(self):
         """Connects to the specified host at the specified port.
-        Raises ConnectionError if not possible."""
+        Raises:
+            ConnectionError if connection fails.
+        """
         try:
             self.server = smtplib.SMTP(self.host, self.port)
         except smtplib.socket.gaierror:
             raise ConnectionError("Error connecting to %s" % (self.host))
 
     def auth(self):
-        """Authendicates with the mail server. Raises MailmanAuthError
-        when the authentication fails."""
+        """Authendicates with the mail server.
+        Raises:
+            MailmanAuthError when the authentication fails.
+        """
         try:
             self.server.login(self.username, self.password)
         except smtplib.SMTPAuthenticationError:
             raise MailmanAuthError("Invalid username (%s) and/or password" % (self.username))
 
     def deliver(self, sender, recipients, message):
-        """."""
+        """Deliver the email message
+        Args:
+            sender (str): who send the message
+            recipients (list): who are the recievers of the message
+            message (str): message to be delivered
+        Raises:
+            MailmanSendError when a SMTPDataError occours
+            ConnectionError when sending times out
+        """
         if recipients is None:
             raise ValueError('You must specified recipients attribute!')
 
